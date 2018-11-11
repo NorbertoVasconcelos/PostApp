7564ed07b3405b17e420ad530715d447423d572f
Norberto Vasconcelos <norberto.vasconcelos@forkit.pt>
2018-11-11 16:56:14 +0000

    Initial xcode project

---
+++ PostApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PostApp
+//
+//  Created by Norberto Vasconcelos on 11/11/2018.
+//  Copyright © 2018 Vasconcelos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
